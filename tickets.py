2d8693c92522c878173463f32768587ad2278b58
yinxiaojian <1529579919@qq.com>
2016-09-22 20:14:49 +0800

    update main module

--- tickets.py
+++ tickets.py
@@ -15,10 +15,19 @@ Example:
 	tickets beijing shanghai 2016-08-25
 """
 from docopt import docopt
+from stations import stations
 def cli():
 	"""command-line interface"""
 	arguments = docopt(__doc__, version='Naval Fate 2.0')
-	print(arguments)
+	from_station = stations.get(arguments['<from>'])
+	to_station = stations.get(arguments['<to>'])
+	date = arguments['<date>']
 
+	#build URL
+	url = 'https://kyfw.12306.cn/otn/lcxxcx/query?purpose_codes=ADULT&queryDate={}&from_station={}&to_station={}'.format(
+		date, from_station, to_station
+	)
+	result = requests.get(url, verify = False)
+	row = r.json()['data']['datas']
 if __name__ == '__main__':
 	cli()
