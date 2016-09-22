3ccef8b497ccbb88c9e06a8673d74975f824e388
yinxiaojian <1529579919@qq.com>
2016-09-22 22:19:59 +0800

    colored the table

---
+++ colored.py
@@ -0,0 +1,10 @@
+def colored(color, text):
+    table = {
+        'red': '\033[91m',
+        'green': '\033[92m',
+        # no color
+        'nc': '\033[0m'
+    }
+    cv = table.get(color)
+    nc = table.get('nc')
+    return ''.join([cv, text, nc])
